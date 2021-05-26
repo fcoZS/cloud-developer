07caafa040288b10b82e80f2941ce3c3b07aa8ae
francisco.zuniga <francis28@gmail.com>
2021-05-26 00:32:12 -0600

    Replace axios with request lib for calling service

--- course-02/exercises/udacity-c2-restapi/src/controllers/v0/image/routes/image.router.ts
+++ course-02/exercises/udacity-c2-restapi/src/controllers/v0/image/routes/image.router.ts
@@ -1,14 +1,25 @@
 import { Router, Request, Response } from 'express';
-import axios from 'axios';
 import { config } from '../../../../config/config';
 
 const router: Router = Router();
+const request = require('request');
 
 router.get('/', async (req: Request, res: Response) => {
   const { image_url } = req.query;
-  const filteredPath = await axios.get(`${config.dev.image_service}/filteredimage?image_url=${image_url}`);
 
-  return res.status(200).send(filteredPath);
+  if (!image_url) {
+      res.status(400).send('bad request: url is required')
+  }
+  request({
+        url: `${config.dev.image_service}/filteredimage?image_url=${image_url}`,
+        encoding: null
+      },
+      (err: any, resp: any, buffer: any) => {
+        if (!err && resp.statusCode === 200){
+          res.set('Content-Type', 'image/jpeg');
+          res.status(200).send(resp.body);
+        }
+      });
 });
 
 export const ImageRouter: Router = router;
